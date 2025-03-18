53932327b95561f927ea625e46760bcfbc8f69fb
Mateusz <mzmiejko5195@gmail.com>
2025-03-18 16:55:41 +0100

    dodane

--- kontrybucja.cpp
+++ kontrybucja.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 int main()
 {
-    for(int i=0;i<100;i++)
+    for(int i=0;i<10;i++)
     {
         cout<<"Witaj swiecie"<<endl;
         cout<<"My name is Bond."<<endl;
     }
+    int n;
+    cout<<"podaj n: ";
+    cin>>n;
+    cout<<"n^2= "<<n*n<<endl;
 
 
     return 0;
